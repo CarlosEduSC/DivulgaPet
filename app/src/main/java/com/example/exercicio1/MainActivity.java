396525a7945bc7bb8d5a7c67a1a076bb1f86b8ad
CarlosEduSC <carloseduardodas.c@gmail.com>
2023-10-26 19:15:57 -0300

    .

--- app/src/main/java/com/example/exercicio1/MainActivity.java
+++ app/src/main/java/com/example/exercicio1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.exercicio1;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.graphics.drawable.LayerDrawable;
@@ -21,7 +23,7 @@ import android.widget.Spinner;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, View.OnClickListener, PopupMenu.OnMenuItemClickListener {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, View.OnClickListener, PopupMenu.OnMenuItemClickListener, AdapterView.OnItemSelectedListener {
     private PopupMenu menu;
     private ImageView imgMenu;
     private Spinner spTipo;
@@ -29,6 +31,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private Spinner spFaixaEtaria;
     private Spinner spSexo;
     private ListView listaPets;
+    private String[] opcoes_tipo;
+    private String[] opcoes_raca_cachorro;
+    private String[] opcoes_raca_gato;
+    private String[] opcoes_faixa_etaria;
+    private String[] opcoes_sexo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,18 +49,71 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         spSexo = findViewById(R.id.spSexo);
         listaPets = findViewById(R.id.lsPets);
 
+        opcoes_tipo = getResources().getStringArray(R.array.tipo);
+        opcoes_raca_cachorro = getResources().getStringArray(R.array.raca_cachorro);
+        opcoes_raca_gato = getResources().getStringArray(R.array.raca_gato);
+        opcoes_faixa_etaria = getResources().getStringArray(R.array.faixa_etaria);
+        opcoes_sexo = getResources().getStringArray(R.array.sexo);
+
         Pet bolaDeNeve = new Pet("Bola de Neve", Pet.Tipo.Cachorro.toString(), Pet.FaixaEtaria.anos35.getDescricao(), Pet.RacaCachorro.poodle.getDescricao(), Pet.Sexo.Macho.toString());
+        Pet bolaDeNeve1 = new Pet("Bola de Neve", Pet.Tipo.Cachorro.toString(), Pet.FaixaEtaria.anos35.getDescricao(), Pet.RacaCachorro.poodle.getDescricao(), Pet.Sexo.Macho.toString());
+        Pet bolaDeNeve2 = new Pet("Bola de Neve", Pet.Tipo.Cachorro.toString(), Pet.FaixaEtaria.anos35.getDescricao(), Pet.RacaCachorro.poodle.getDescricao(), Pet.Sexo.Macho.toString());
+        Pet bolaDeNeve3 = new Pet("Bola de Neve", Pet.Tipo.Cachorro.toString(), Pet.FaixaEtaria.anos35.getDescricao(), Pet.RacaCachorro.poodle.getDescricao(), Pet.Sexo.Macho.toString());
+        Pet bolaDeNeve4 = new Pet("Bola de Neve", Pet.Tipo.Cachorro.toString(), Pet.FaixaEtaria.anos35.getDescricao(), Pet.RacaCachorro.poodle.getDescricao(), Pet.Sexo.Macho.toString());
+        Pet bolaDeNeve5 = new Pet("Bola de Neve", Pet.Tipo.Cachorro.toString(), Pet.FaixaEtaria.anos35.getDescricao(), Pet.RacaCachorro.poodle.getDescricao(), Pet.Sexo.Macho.toString());
+        Pet bolaDeNeve6 = new Pet("Bola de Neve", Pet.Tipo.Cachorro.toString(), Pet.FaixaEtaria.anos35.getDescricao(), Pet.RacaCachorro.poodle.getDescricao(), Pet.Sexo.Macho.toString());
+        Pet bolaDeNeve7 = new Pet("Bola de Neve", Pet.Tipo.Cachorro.toString(), Pet.FaixaEtaria.anos35.getDescricao(), Pet.RacaCachorro.poodle.getDescricao(), Pet.Sexo.Macho.toString());
+        Pet bolaDeNeve8 = new Pet("Bola de Neve", Pet.Tipo.Cachorro.toString(), Pet.FaixaEtaria.anos35.getDescricao(), Pet.RacaCachorro.poodle.getDescricao(), Pet.Sexo.Macho.toString());
+        Pet bolaDeNeve9 = new Pet("Bola de Neve", Pet.Tipo.Cachorro.toString(), Pet.FaixaEtaria.anos35.getDescricao(), Pet.RacaCachorro.poodle.getDescricao(), Pet.Sexo.Macho.toString());
+        Pet bolaDeNeve10 = new Pet("Bola de Neve", Pet.Tipo.Cachorro.toString(), Pet.FaixaEtaria.anos35.getDescricao(), Pet.RacaCachorro.poodle.getDescricao(), Pet.Sexo.Macho.toString());
 
         List<Pet> pets = new ArrayList<Pet>();
 
         pets.add(bolaDeNeve);
+        pets.add(bolaDeNeve1);
+        pets.add(bolaDeNeve2);
+        pets.add(bolaDeNeve3);
+        pets.add(bolaDeNeve4);
+        pets.add(bolaDeNeve5);
+        pets.add(bolaDeNeve6);
+        pets.add(bolaDeNeve7);
+        pets.add(bolaDeNeve8);
+        pets.add(bolaDeNeve9);
+        pets.add(bolaDeNeve10);
         imgMenu.setOnClickListener(this);
         PetsAdapter petsAdapter = new PetsAdapter(this, pets);
         listaPets.setAdapter(petsAdapter);
 
         listaPets.setOnItemClickListener(this);
 
-        opcoesSpinners();
+        Spinners();
+
+        spTipo.setOnItemSelectedListener(this);
+        spRaca.setOnItemSelectedListener(this);
+        spFaixaEtaria.setOnItemSelectedListener(this);
+        spSexo.setOnItemSelectedListener(this);
+    }
+
+    private void Spinners() {
+            ArrayAdapter<String> adapterTipo = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opcoes_tipo);
+            adapterTipo.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spTipo.setAdapter(adapterTipo);
+
+            ArrayList<String> raca = new ArrayList<String>();
+            raca.add("Raça");
+            raca.add("Selecione o tipo do animal primeiro!");
+
+            ArrayAdapter<String> adapterRaca = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, raca);
+            adapterRaca.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spRaca.setAdapter(adapterRaca);
+
+            ArrayAdapter<String> adapterFaixaEtaria = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opcoes_faixa_etaria);
+            adapterFaixaEtaria.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spFaixaEtaria.setAdapter(adapterFaixaEtaria);
+
+            ArrayAdapter<String> adapterSexo = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opcoes_sexo);
+            adapterSexo.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spSexo.setAdapter(adapterSexo);
     }
 
     private void OpenMenu(View view) {
@@ -67,37 +127,26 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         menu.show();
     }
 
-    private void opcoesSpinners() {
-        String[] opcoes_tipo = getResources().getStringArray(R.array.tipo);
-        String[] opcoes_raca_cachorro = getResources().getStringArray(R.array.raca_cachorro);
-        String[] opcoes_raca_gato = getResources().getStringArray(R.array.raca_gato);
-        String[] opcoes_faixa_etaria = getResources().getStringArray(R.array.faixa_etaria);
-        String[] opcoes_sexo = getResources().getStringArray(R.array.sexo);
-
-
-        ArrayAdapter<String> adapterTipo = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opcoes_tipo);
-        adapterTipo.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spTipo.setAdapter(adapterTipo);
-
-
-        ArrayAdapter<String> adapterRaca = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opcoes_raca_cachorro);
-        adapterRaca.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spRaca.setAdapter(adapterRaca);
-
-        ArrayAdapter<String> adapterFaixaEtaria = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opcoes_faixa_etaria);
-        adapterFaixaEtaria.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spFaixaEtaria.setAdapter(adapterFaixaEtaria);
-
-        ArrayAdapter<String> adapterSexo = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opcoes_sexo);
-        adapterSexo.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spSexo.setAdapter(adapterSexo);
-    }
-
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-        Intent intent = new Intent(MainActivity.this, DetalharActivity.class);
+        if (view == listaPets) {
+            Intent intent = new Intent(MainActivity.this, DetalharActivity.class);
 
-        startActivity(intent);
+            startActivity(intent);
+
+        } else if (view == spTipo) {
+            if (spTipo.getSelectedItem() == opcoes_tipo[1]) {
+                ArrayAdapter<String> adapterRaca = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opcoes_raca_cachorro);
+                adapterRaca.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spRaca.setAdapter(adapterRaca);
+
+            } else if (spTipo.getSelectedItem() == opcoes_tipo[2]) {
+                ArrayAdapter<String> adapterRaca = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opcoes_raca_gato);
+                adapterRaca.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spRaca.setAdapter(adapterRaca);
+
+            }
+        }
     }
 
     @Override
@@ -125,5 +174,35 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             startActivity(intent);
         }
         return false;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        int id = adapterView.getId();
+
+        if (id ==R.id.spTipo) {
+            if (spTipo.getSelectedItem() == opcoes_tipo[1]) {
+                ArrayAdapter<String> adapterRaca = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opcoes_raca_cachorro);
+                adapterRaca.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spRaca.setAdapter(adapterRaca);
+
+            } else if (spTipo.getSelectedItem() == opcoes_tipo[2]) {
+                ArrayAdapter<String> adapterRaca = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opcoes_raca_gato);
+                adapterRaca.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spRaca.setAdapter(adapterRaca);
+
+            }
+
+        } else if (id ==R.id.spRaca) {
+
+        } else if (id ==R.id.spFaixaEtaria) {
+
+        } else if (id ==R.id.spSexo) {
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
     }
 }
